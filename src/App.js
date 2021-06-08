93934fd5179e52757a8406610c9d33e2ceddc0c5
teoteo123 <theo8pizza@gmail.com>
2021-06-08 14:36:31 -0400

    Card generation working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardPage />
     </div>
   );
-}
+};
+
+const CardPage = (props) => {
+	const images = [
+		1,2,3,4,5,6,7,8,9,10,11
+	]
+  return (
+    <div>
+      <div className="main">
+        <h1>Responsive Card Grid Layout</h1>
+        <ul className="cards">
+			{
+				images.map((val, i) => {
+					return <Card imgLink={'https://picsum.photos/500/300/?img=' + val} />
+				})
+			}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Card = (props) => {
+  return (
+    <li className="cards_item">
+      <div className="card">
+        <div className="card_image">
+          <img src={props.imgLink || "https://picsum.photos/500/300/"} />
+        </div>
+        <div className="card_content">
+          <h2 className="card_title">{props.title || "Card Grid Layout"}</h2>
+          <p className="card_text">
+            {props.description || "Demo of pixel perfect pure CSS simple responsive card grid layout"}
+          </p>
+          <button className="btn card_btn">Check it out</button>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default App;
